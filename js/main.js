4a112c8e6dbd8514c349fa053ca7cb009a7caf18
panickylemon <alenka__1992@mail.ru>
2017-01-12 01:41:05 +0300

    footer

--- js/main.js
+++ js/main.js
@@ -37,4 +37,16 @@ $(document).ready(function ($) {
         }
     });
 
+
+
+    $(document).ready(function() {
+        $('a[href^="#"]').click(function(){
+            var el = $(this).attr('href');
+            $('body').animate({
+                scrollTop: $(el).offset().top}, 1000);
+            return false;
+        });
+    });
+
+
 });
